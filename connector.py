7f78bf539ad2dcc4a3965a07eb6e5d97ff77ca3b
Logan Lessen <loganlessen@gmail.com>
2025-02-19 12:52:58 -0700

    Added connection functionality

--- connector.py
+++ connector.py
@@ -4,12 +4,23 @@ import socket
 def host_game(num_players):
     return Server(socket.socket(socket.AF_INET, socket.SOCK_STREAM), num_players)
 
+def connect(ip):
+    return Client(socket.socket(socket.AF_INET, socket.SOCK_STREAM), ip)
 
 class Client:
-    pass
+    def __init__(self, sock, ip):
+        self.socket = sock
+        self.socket.connect((ip, 1212))
+        print("Connected to server")
 
 class Server:
     def __init__(self, sock, player_count):
         self.socket = sock
         self.socket.bind(("0.0.0.0", 1212))
         self.socket.listen(player_count)
+        print(f"Players connected 1/{player_count}")
+        for i in range(1, player_count):
+            conn, addr = self.socket.accept()
+            print(f"\nPlayer {i + 1} connected")
+            print(f"Players connected {i + 1}/{player_count}")
+        print("All players connected")
--- game.py
+++ game.py
@@ -2,11 +2,11 @@ import connector
 import graphics
 import manager
 
-def prompt(text = "", options = []):
+def prompt(text = "", options = [], error = ""):
     print(text)
     user_input = input()
     if options and (user_input not in options):
-        print("\nPlease choose a valid option\n")
+        print(error if error else "\nPlease choose a valid option\n")
         user_input = prompt(text, options)
     return user_input
 
@@ -15,4 +15,9 @@ print("Welcome to ____\n")
 is_hosting = prompt("Are you hosting or joining a game?\n1. Hosting\n2. Joining", ["1", "2"]) == "1"
 
 if is_hosting:
-    player = connector.host_game()
+    player_count = int(prompt("How many people are playing? Choose a number between 2 and 4", ["2", "3", "4"], "\nPlease chose a player count between 2 and 4\n"))
+    player = connector.host_game(player_count)
+else:
+    print("What IP address do you want to connect to?")
+    ip = input()
+    player = connector.connect(ip)
